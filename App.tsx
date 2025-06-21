db52049dd2fd5ad14daedbf19e847d250601592e
Saurabh Kumar <patiramproduction@gmail.com>
2025-06-22 01:38:59 +0530

    create main

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import MainComponent from './src/screens/main';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <MainComponent />
   );
 }
 
---
+++ src/screens/main.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function MainComponent() {
+    return (
+        <View >
+            <Text>Hello, this is a functional component!</Text>
+        </View>
+    );
+}
+
+export default MainComponent;
