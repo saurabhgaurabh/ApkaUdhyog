d8e96cec90d54dc8cfd143660b6caf484667753e
Saurabh Kumar <patiramproduction@gmail.com>
2025-07-17 23:57:50 +0530

    create splash route

---
+++ src/navigation/SplashRoutes.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const splashRoutes = () => {
+  return (
+    <View>
+      <Text>splashRoutes</Text>
+    </View>
+  )
+}
+
+export default splashRoutes
--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -1,15 +1,15 @@
 import react from 'react';
 import { View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
+import SplashRoutes from '../navigation/SplashRoutes';
 
 function MainComponent() {
     return (
-        <View style={{ flex: 1, backgroundColor: 'red' }}>
-            <Text>Hello, this is a functional component!</Text>
+        <View style={{ flex: 1 }}>
+            <SplashRoutes />
         </View>
     );
-}   
+}
 
 export default MainComponent;
 
