7a2aef854ebb139e07b2422da8bd999acdcf5c92
Saurabh Kumar <patiramproduction@gmail.com>
2025-08-20 23:12:15 +0530

    create home in homestack

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Settings from '../navigation/Drawer/SettingsStack'
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View>
       <Text>Home</Text>
+      <View><Text>go to settings </Text></View>
+      <TouchableOpacity onPress={() => navigation.navigate('Settings')}><Text>go to settings </Text></TouchableOpacity>
     </View>
   )
 }
